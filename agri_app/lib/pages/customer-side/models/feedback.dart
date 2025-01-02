9a22dfba53a7460d7cf585ba033ec7a13b4471d3
Nielvin Amancio <112628938+Amanciolicious@users.noreply.github.com>
2025-01-02 14:04:51 +0800

    Add files via upload

---
+++ agri_app/lib/pages/customer-side/models/feedback.dart
@@ -0,0 +1,12 @@
+// feedback_model.dart
+class FeedbackModel {
+  final String id;
+  final String message;
+  final DateTime timestamp;
+
+  FeedbackModel({
+    required this.id,
+    required this.message,
+    required this.timestamp,
+  });
+}
